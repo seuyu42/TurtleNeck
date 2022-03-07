9c086b3b2553cd765e3c8dc40eaf0688fea791d1
seuyu <tmdgus4350@gmail.com>
2022-03-07 17:41:59 +0900

    Initial Commit

---
+++ TurtleNeck/TurtleNeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurtleNeckApp.swift
+//  TurtleNeck
+//
+//  Created by yu seunghyun on 2022/03/07.
+//
+
+import SwiftUI
+
+@main
+struct TurtleNeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
